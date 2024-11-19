768d518fb4da2332001573958422aad77c7ea3cb
jenkins_ci <jenkins_ci@limxdynamics.com>
2024-11-19 16:32:48 +0800

    1.1.5.20241119160355

--- pointfoot_gazebo/launch/empty_world.launch.py
+++ pointfoot_gazebo/launch/empty_world.launch.py
@@ -53,7 +53,7 @@ def generate_launch_description():
             package='gazebo_ros', 
             executable='spawn_entity.py', 
             arguments=['-topic', 'robot_description', '-entity', 'pointfoot_entity', 
-                      '-x', '0', '-y', '0', '-z', '0.62'],
+                      '-x', '0', '-y', '0', '-z', '0.82'],
             output='screen'
         ),
         Node(
